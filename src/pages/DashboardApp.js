6dda524840285b9e44636bc4aa4b59b4a7bdc9ec
Terence <z5311419@student.unsw.edu.au>
2021-08-28 01:15:53 +1000

    starting on profile

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -16,16 +16,31 @@ import {
   AppCurrentSubject,
   AppConversionRates
 } from '../components/_dashboard/app';
-
+import { useEffect } from 'react';
+import axios from 'axios';
 // ----------------------------------------------------------------------
 
 export default function DashboardApp() {
+  // useEffect(async () => {
+  //   axios.get('https://zorlvan-enterprise-backend.herokuapp.com/account/profile', {
+  //     "user_id": 2
+  //   }, null)
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+  // }, [])
+
+
   return (
     <Page title="Dashboard | Minimal-UI">
       <Container maxWidth="xl">
         <Box sx={{ pb: 5 }}>
           <Typography variant="h4">Hi, Welcome back</Typography>
         </Box>
+
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
             <AppWeeklySales />
