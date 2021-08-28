c5aa0540b6b5551d99412577271e01659dd23d95
Sophie <sophiewang0409@gmail.com>
2021-08-29 00:25:32 +1000

    Added Thank You + Event Stub

---
+++ src/pages/Events.js
@@ -0,0 +1,79 @@
+import { useFormik } from 'formik';
+import { useState } from 'react';
+// material
+import { Container, Stack, Typography } from '@material-ui/core';
+// components
+import Page from '../components/Page';
+import {
+  ProductSort,
+  ProductList,
+  ProductCartWidget,
+  ProductFilterSidebar
+} from '../components/_dashboard/products';
+//
+import products from '../_mocks_/products';
+
+// ----------------------------------------------------------------------
+
+export default function Events() {
+  const [openFilter, setOpenFilter] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      gender: '',
+      category: '',
+      colors: '',
+      priceRange: '',
+      rating: ''
+    },
+    onSubmit: () => {
+      setOpenFilter(false);
+    }
+  });
+
+  const { resetForm, handleSubmit } = formik;
+
+  const handleOpenFilter = () => {
+    setOpenFilter(true);
+  };
+
+  const handleCloseFilter = () => {
+    setOpenFilter(false);
+  };
+
+  const handleResetFilter = () => {
+    handleSubmit();
+    resetForm();
+  };
+
+  return (
+    <Page title="Events">
+      <Container>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Get involved in with the PhilGood community! 
+        </Typography>
+
+        <Stack
+          direction="row"
+          flexWrap="wrap-reverse"
+          alignItems="center"
+          justifyContent="flex-end"
+          sx={{ mb: 5 }}
+        >
+          <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
+            <ProductFilterSidebar
+              formik={formik}
+              isOpenFilter={openFilter}
+              onResetFilter={handleResetFilter}
+              onOpenFilter={handleOpenFilter}
+              onCloseFilter={handleCloseFilter}
+            />
+            {/*<ProductSort />*/}
+          </Stack>
+        </Stack>
+        {console.log(products)}
+        <ProductList products={products} />
+      </Container>
+    </Page>
+  );
+}
